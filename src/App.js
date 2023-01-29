3b86d064d637710e401619f83422b5ff6754e282
VISHWANATH-1 <vishwanathpandey11@gmail.com>
2023-01-29 19:08:16 +0530

    compleated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import {useEffect , useState} from "react";
 
-function App() {
+const baseURL = "https://jsonplaceholder.typicode.com/posts";
+
+export default function App() {
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    axios.get(`${baseURL}/1`).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  function createPost() {
+    axios
+      .post(baseURL, {
+        title: "Hello World!",
+        body: "This is a new post."
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  if (!post) return "No post!"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <button onClick={createPost}>Create Post</button>
     </div>
   );
 }
-
-export default App;
